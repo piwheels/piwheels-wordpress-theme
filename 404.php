be3f39f2b3790dd2b52f1e660cc4316bf82bf0de
Ben Nuttall <ben@bennuttall.com>
2020-05-30 21:04:06 +0100

    Add 404 page template

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<div class="row">
+  <div class="small-12 medium-8 columns">
+    <h1>piwheels blog</h1>
+  </div>
+  <div class="small-12 medium-8 columns">
+    <h2>404 &mdash; page not found</h2>
+    <p>No content was found at this location. Try the <a href="/">blog homepage</a> where all posts are featured, or head back to the <a href="https://www.piwheels.org/">piwheels homepage</a> for more.</p>
+  </div>
+  <div></div>
+
+</div>
+
+<?php get_footer();

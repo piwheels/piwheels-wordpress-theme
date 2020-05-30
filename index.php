ea1d1e1dc6e302a06aae7b9e9199d870a47d0806
Ben Nuttall <ben@bennuttall.com>
2020-05-30 21:03:58 +0100

    Align h1 properly

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php get_header(); ?>
 
 <div class="row">
+  <div class="small-12 medium-8 columns">
     <h1>piwheels blog</h1>
+  </div>
 <?php if (have_posts()): ?>
-    <?php while (have_posts()): the_post(); ?>
-    <div class="small-12 medium-8 columns">
-        <date><?php echo get_the_date(); ?></date>
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <?php the_excerpt(); ?>
-        <div class="callout primary">
-            <?php show_tag_list(get_the_ID(), ', ', 'Tags: '); ?>
-        </div>
+  <?php while (have_posts()): the_post(); ?>
+  <div class="small-12 medium-8 columns">
+    <date><?php echo get_the_date(); ?></date>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php the_excerpt(); ?>
+    <div class="callout primary">
+      <?php show_tag_list(get_the_ID(), ', ', 'Tags: '); ?>
     </div>
-    <?php endwhile; ?>
+  </div>
+  <?php endwhile; ?>
 <?php else: ?>
-    <div class="small-12 medium-8 columns">
-        <h2>No posts</h2>
-    </div>
+  <div class="small-12 medium-8 columns">
+    <h2>No posts</h2>
+  </div>
 <?php endif; ?>
 <div></div>
 

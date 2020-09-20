73df1bc225f4f75bb7149c1f09b3f57760891b61
Ben Nuttall <ben@bennuttall.com>
2020-09-20 15:33:16 +0000

    Add missing post-meta file

---
+++ post-meta.php
@@ -0,0 +1,4 @@
+<div class="callout primary">
+    <strong>Author:</strong> <a href="https://twitter.com/<?php the_author_meta('twitter'); ?>"><?php the_author(); ?></a>
+    <?php show_tag_list(get_the_ID(), ', ', '<strong>Tags:</strong> '); ?>
+</div>
